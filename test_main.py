487b83d7d46c040826da913f8c72daab2737b3cd
mgdmrk <mgdmrk@users.noreply.github.com>
2020-06-29 01:10:48 +0200

    test update

---
+++ test_main.py
@@ -0,0 +1,19 @@
+import unittest
+import pygame
+
+import main
+import assets
+
+class EnemyTest(unittest.TestCase):
+
+    def setUp(self):
+        self.enemy = main.Enemy(200, 300, "pink")
+        assets.Assets.load()
+
+    def test_move(self):
+        self.enemy.move(5)
+        self.assertEqual(self.enemy.y, 300 + 5)
+
+if __name__ == '__main__':
+    unittest.main()
+
